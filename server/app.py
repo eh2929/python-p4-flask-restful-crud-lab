9fc0e6f370e98a32f2c9b995f34d0d12ddbe52b0
Ethan <ethanhough29@gmail.com>
2024-02-23 10:59:48 -0500

    complete lab

--- server/app.py
+++ server/app.py
@@ -7,8 +7,8 @@ from flask_restful import Api, Resource
 from models import db, Plant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///plants.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -27,9 +27,9 @@ class Plants(Resource):
         data = request.get_json()
 
         new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
+            name=data["name"],
+            image=data["image"],
+            price=data["price"],
         )
 
         db.session.add(new_plant)
@@ -38,7 +38,7 @@ class Plants(Resource):
         return make_response(new_plant.to_dict(), 201)
 
 
-api.add_resource(Plants, '/plants')
+api.add_resource(Plants, "/plants")
 
 
 class PlantByID(Resource):
@@ -47,9 +47,47 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant is None:
+            return make_response({"error": "Plant not found"}, 404)
 
-api.add_resource(PlantByID, '/plants/<int:id>')
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response("", 204)
+
+    # PATCH deliverable
+
+    def patch(self, id):
+        data = Plant.query.filter_by(id=id).first()
+        if data is None:
+            return make_response({"error": "Plant not found"}, 404)
+
+        json_data = request.get_json()
+        for attr in json_data:
+            setattr(data, attr, json_data[attr])
+
+        db.session.add(data)
+        db.session.commit()
+        response_dict = data.to_dict()
+        response = make_response(response_dict, 200)
+        return response
+
+    # # DELETE deliverable
 
 
-if __name__ == '__main__':
+# def delete(self, id):
+#     plant = Plant.query.filter_by(id=id).first()
+#     if plant is None:
+#         return make_response({"error": "Plant not found"}, 404)
+
+#     db.session.delete(plant)
+#     db.session.commit()
+#     return make_response("", 204)
+
+
+api.add_resource(PlantByID, "/plants/<int:id>")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
